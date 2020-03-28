73c35e12c29a20cf0b070aefa1b8f35b0069191b
Jasper Janboy Monterey <janboym@gmail.com>
2020-03-28 17:14:02 +0800

    Updates

---
+++ precache-manifest.f8786ea2f1699b20d5f88ffb09864361.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93c442e7578bb2e3940262810cf773c",
+    "url": "/RBIELsBistro/index.html"
+  },
+  {
+    "revision": "47551d8f86b87507cdf8",
+    "url": "/RBIELsBistro/static/css/2.cbab55cf.chunk.css"
+  },
+  {
+    "revision": "063d31be87b938e6c3f4",
+    "url": "/RBIELsBistro/static/css/main.752c3244.chunk.css"
+  },
+  {
+    "revision": "47551d8f86b87507cdf8",
+    "url": "/RBIELsBistro/static/js/2.54eee3a0.chunk.js"
+  },
+  {
+    "revision": "06cbae36bbec21d42c06d5f2ca679947",
+    "url": "/RBIELsBistro/static/js/2.54eee3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063d31be87b938e6c3f4",
+    "url": "/RBIELsBistro/static/js/main.576b32cd.chunk.js"
+  },
+  {
+    "revision": "7832f404830cc2c0b5ab",
+    "url": "/RBIELsBistro/static/js/runtime-main.b584f310.js"
+  },
+  {
+    "revision": "96e799f5bcb0a0097e709fca76e25f58",
+    "url": "/RBIELsBistro/static/media/3.96e799f5.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/RBIELsBistro/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/RBIELsBistro/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/RBIELsBistro/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "90285d6f41d88b4ea7bc31525d7cb3bd",
+    "url": "/RBIELsBistro/static/media/bg-dark-gray-pattern.90285d6f.jpg"
+  },
+  {
+    "revision": "d6fb37394fce1da08d92fd9ce7ff0e4d",
+    "url": "/RBIELsBistro/static/media/chef.d6fb3739.jpg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/RBIELsBistro/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/RBIELsBistro/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/RBIELsBistro/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/RBIELsBistro/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/RBIELsBistro/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/RBIELsBistro/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/RBIELsBistro/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/RBIELsBistro/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/RBIELsBistro/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/RBIELsBistro/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/RBIELsBistro/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/RBIELsBistro/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/RBIELsBistro/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/RBIELsBistro/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/RBIELsBistro/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "65d7b057fd4ef34362d23fd9d8f8f7c9",
+    "url": "/RBIELsBistro/static/media/floor1.65d7b057.jpg"
+  },
+  {
+    "revision": "9cc8fe897452ba3578dd23bf150cc8ec",
+    "url": "/RBIELsBistro/static/media/floor2.9cc8fe89.jpg"
+  },
+  {
+    "revision": "f1385d148c18fcfe69ecfe53b57ceda9",
+    "url": "/RBIELsBistro/static/media/floor3.f1385d14.jpg"
+  },
+  {
+    "revision": "47818b9f9c973c48e649f819dd990a2b",
+    "url": "/RBIELsBistro/static/media/introbg.47818b9f.jpg"
+  },
+  {
+    "revision": "5b405ab0451da4b239b138631b8d0b6b",
+    "url": "/RBIELsBistro/static/media/rowbg-five-img.5b405ab0.jpg"
+  },
+  {
+    "revision": "2c122b9a66ab4fec918422af607a7d08",
+    "url": "/RBIELsBistro/static/media/sub-banner.2c122b9a.jpg"
+  },
+  {
+    "revision": "ec9a2d4a28f7d7c55061f219ac3c9c34",
+    "url": "/RBIELsBistro/static/media/testimonials-bg.ec9a2d4a.jpg"
+  },
+  {
+    "revision": "fd75ee10c580b5d958390ad07538b02b",
+    "url": "/RBIELsBistro/static/media/titlebar-bg-img.fd75ee10.jpg"
+  }
+]);
